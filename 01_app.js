0343921259bfd9ab9084c55ac044ca5306ebee7d
valp26 <valp26@outlook.com>
2018-02-08 09:17:39 -0500

    Ajout du courriel et du telephone dans l'objet reponse

--- 01_app.js
+++ 01_app.js
@@ -19,7 +19,9 @@ console.log('la route /traiter_get')
 // on utilise l'objet req.query pour récupérer les données GET
  let reponse = {
  prenom:req.query.prenom,
- nom:req.query.nom
+ nom:req.query.nom,
+ courriel:req.query.courriel,
+ telephone:req.query.telephone
  };
 console.log(reponse);
  res.end(JSON.stringify(reponse));
